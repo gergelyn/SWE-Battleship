5ac0ed706a25ab0007c9dd8523d0513936dc4caa
gergelyn <preszthegas@gmail.com>
2022-04-10 16:57:07 +0200

    ShipType enum

---
+++ src/main/java/model/ShipType.java
@@ -0,0 +1,19 @@
+package model;
+
+public enum ShipType {
+    DESTROYER(2),
+    SUBMARINE(3),
+    CRUISER(3),
+    BATTLESHIP(4),
+    CARRIER(5)
+
+    int shipLength;
+
+    ShipType(int shipLength) {
+        this.shipLength = shipLength;
+    }
+
+    public int getShipLength() {
+        return shipLength;
+    }
+}
